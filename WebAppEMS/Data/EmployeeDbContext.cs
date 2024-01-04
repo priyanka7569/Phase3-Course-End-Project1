46f16b86f7bfe32b47d3d2b61c6941a4f2161446
priyanka7569 <kota.priyanka@mphasis.com>
2024-01-04 13:37:19 +0530

    Course End Project phase3 modified

---
+++ WebAppEMS/Data/EmployeeDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAppEMS.Models;
+
+namespace WebAppEMS.Data
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public EmployeeDbContext (DbContextOptions<EmployeeDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebAppEMS.Models.EmpProfile> EmpProfile { get; set; } = default!;
+
+        public DbSet<WebAppEMS.Models.DeptMaster>? DeptMaster { get; set; }
+    }
+}
---
+++ WebAppEMS/Models/DeptMaster.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAppEMS.Models
+{
+    [Table("DeptMaster")]
+    public class DeptMaster
+    {
+        [Key]
+        public int DeptCode { get; set; }
+        public string? DeptName { get; set; }
+        public virtual ICollection<EmpProfile>? EmpProfiles { get; set; }
+
+    }
+}
